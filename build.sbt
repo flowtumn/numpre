eb826080949f20aa3c85dab7edf9933b98d9875b
flowTumn <air_bl03@yahoo.co.jp>
2015-09-12 16:57:43 +0900

    add sbt.

---
+++ build.sbt
@@ -0,0 +1,4 @@
+name := "numpre"
+version := "0.0"
+scalaVersion in ThisBuild := "2.11.6"
+libraryDependencies += "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
---
+++ src/test/scala/jp/flowtumn/numpre/helloTest.scala
@@ -0,0 +1,9 @@
+package jp.flowtumn.numpre
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.Matchers
+
+class HelloTest extends FunSuite with Matchers {
+	test("hoge") {
+	}
+}
